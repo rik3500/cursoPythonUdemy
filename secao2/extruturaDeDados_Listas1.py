a73a2bbefaf931a5d33e96f40d01ddf35e86b071
Ricardo Hatsugai <rhatsugai@gmai.com>
2021-04-18 06:07:56 -0300

    Extruturas de dados parte 1

---
+++ secao2/extruturaDeDados_Listas1.py
@@ -0,0 +1,21 @@
+'''
+append
+insert(0,'x')
+pop(l)
+remove('x')
+len()
+sort()
+reverse
+
+max()
+min()
+sum()
+'''
+
+x = [] #declaração de uma lista vazia.
+
+idade = [20, 21, 30, 40]
+
+idade.append(18)
+
+print(idade)
