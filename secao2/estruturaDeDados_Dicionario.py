a269d62e56d151d4acdd3819518350ddb65a2a21
Ricardo Hatsugai <rhatsugai@gmai.com>
2021-04-22 16:54:30 -0300

    Estrutura de dados Dicionarios2

---
+++ secao2/estruturaDeDados_Dicionario.py
@@ -0,0 +1,9 @@
+x = {'nome' : 'Ricardo', 'idade' : 46, 'Telefone' : '62 98113 3558'}
+
+x.pop('idade') # removeu o índice idade.
+
+print(x) #imprime todo dicionário
+
+print(x['Telefone']) # imprime o telefone do primeiro índice.
+
+print(len(x)) # imprime os três valores ou quantidade de elementos.
---
+++ secao2/estruturaDeDados_Dicionario2.py
@@ -0,0 +1,9 @@
+cadastroPessoas = [{'nome':'Ricardo', 'idade':18, 'telefone':'62981133558', 'cpf':'545442454524'}, {'nome':'Elisângela', 'idade':15, 'telefone':'62984813844', 'cpf':'5454424444424'}]
+
+print(cadastroPessoas) #mostra toda lista de dicionário
+
+print(cadastroPessoas[1]) # mostra o cadastro 2 da Elisângela
+
+print(cadastroPessoas[1]['telefone']) # Mostra o telefone do cadastro 2.
+
+print(cadastroPessoas[1]['nome'])
