b6cb430d868032e21ae58bbe61ca00033adea565
Ricardo Hatsugai <rhatsugai@gmai.com>
2021-04-24 10:42:03 -0300

    Funções em python

---
+++ secao2/funcoes.py
@@ -0,0 +1,4 @@
+def retornoMaior(*list):
+    print(max(list))
+
+retornoMaior(1, 2, 3, 4, 100)
---
+++ secao2/funcoes2.py
@@ -0,0 +1,5 @@
+def precoLiquido(preco,desc):
+    resultado=(preco-(preco*(desc/100)))
+    print(resultado)
+
+precoLiquido(5.60,50)
