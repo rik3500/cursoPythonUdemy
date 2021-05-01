2ddb6d45f006f3bd387dde0018511283313b281d
Ricardo Hatsugai <rhatsugai@gmai.com>
2021-05-01 08:33:20 -0300

    seção-3 criando banco de dados e tabelas

---
+++ secao3/tabelas.py
@@ -0,0 +1,19 @@
+'''
+create database livraria;
+use livraria;
+
+create table pessoas(
+    nome varchar(50),
+    idade int,
+    cep varchar(20),
+    cpf varchar(20)
+);
+
+create table livro(
+    nome varchar(100),
+    genero varchar(50),
+    preco real
+);
+
+
+'''
