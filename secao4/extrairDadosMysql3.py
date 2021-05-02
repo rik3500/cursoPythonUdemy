0ad6378e72aa54877df34e1ddd2c7ab050aa31f1
Ricardo Hatsugai <rhatsugai@gmai.com>
2021-05-01 22:30:54 -0300

    Extraindo dados do banco de dados Mysql com o Python

---
+++ secao4/extrairDadosMysql3.py
@@ -0,0 +1,18 @@
+import pymysql.cursors
+
+conexao = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='',
+    db='interacaopython',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
+with conexao.cursor() as cursor:
+    cursor.execute('select * from cadastros')
+    resultado = cursor.fetchall()
+
+for dado in resultado:
+    print('O nome do cliente é {} e mora no endereço {}'.format(dado['nome'], dado['endereco']))
